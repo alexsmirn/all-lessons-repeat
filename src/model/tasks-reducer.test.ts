9df81100cbdd492b25df58f98a706bb0e8e566d9
alexsmirn <alexsmirnoffprogramming@gmail.com>
2024-06-12 16:46:43 +0300

    lesson-09 (6/10done) commit

--- src/model/tasks-reducer.test.ts
+++ src/model/tasks-reducer.test.ts
@@ -1,31 +1,22 @@
 import {v1} from "uuid";
-import {removeTaskAC, tasksReducer} from "./tasks-reducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitle, removeTaskAC, tasksReducer} from "./tasks-reducer";
+
+//Initial state
+const todolistId1 = v1()
+const todolistId2 = v1()
+
+const initialState = {
+    [todolistId1]: [
+        {id: '1', title: 'Todolist N1 Task N1', isDone: false},
+        {id: '2', title: 'Todolist N1 Task N2', isDone: false},
+    ],
+    [todolistId2]: [
+        {id: '1', title: 'Todolist N2 Task N1', isDone: false},
+        {id: '2', title: 'Todolist N2 Task N2', isDone: false},
+    ]
+}
 
 test('correct task should be deleted from correct todolist', () => {
-
-    //Initial state
-    const todolistId1 = v1()
-    const todolistId2 = v1()
-
-    const initialState = {
-        [todolistId1]: [
-            {id: '1', title: 'Todolist N1 Task N1', isDone: false},
-            {id: '2', title: 'Todolist N1 Task N2', isDone: false},
-            {id: '3', title: 'Todolist N1 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N4', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N5', isDone: true},
-            {id: '5', title: 'Todolist N1 Task N6', isDone: true},
-        ],
-        [todolistId2]: [
-            {id: '1', title: 'Todolist N2 Task N1', isDone: false},
-            {id: '2', title: 'Todolist N2 Task N2', isDone: false},
-            {id: '3', title: 'Todolist N2 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N2 Task N4', isDone: true},
-            {id: '5', title: 'Todolist N2 Task N5', isDone: true},
-            {id: '6', title: 'Todolist N2 Task N6', isDone: true},
-        ]
-    }
-
     const action = removeTaskAC(todolistId1, '2')
 
     const resultState = tasksReducer(initialState, action)
@@ -33,45 +24,38 @@ test('correct task should be deleted from correct todolist', () => {
     expect(resultState).toEqual({
         [todolistId1]: [
             {id: '1', title: 'Todolist N1 Task N1', isDone: false},
-            {id: '3', title: 'Todolist N1 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N4', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N5', isDone: true},
-            {id: '5', title: 'Todolist N1 Task N6', isDone: true},
         ],
         [todolistId2]: [
             {id: '1', title: 'Todolist N2 Task N1', isDone: false},
             {id: '2', title: 'Todolist N2 Task N2', isDone: false},
-            {id: '3', title: 'Todolist N2 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N2 Task N4', isDone: true},
-            {id: '5', title: 'Todolist N2 Task N5', isDone: true},
-            {id: '6', title: 'Todolist N2 Task N6', isDone: true},
         ]
     })
 })
 
 test('correct task should be added to correct todolist', () => {
-    //Initial state
-    const todolistId1 = v1()
-    const todolistId2 = v1()
+    const newTaskTitle = 'New task (tasksReducer)'
 
-    const initialState = {
-        [todolistId1]: [
-            {id: '1', title: 'Todolist N1 Task N1', isDone: false},
-            {id: '2', title: 'Todolist N1 Task N2', isDone: false},
-            {id: '3', title: 'Todolist N1 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N4', isDone: true},
-            {id: '4', title: 'Todolist N1 Task N5', isDone: true},
-            {id: '5', title: 'Todolist N1 Task N6', isDone: true},
-        ],
-        [todolistId2]: [
-            {id: '1', title: 'Todolist N2 Task N1', isDone: false},
-            {id: '2', title: 'Todolist N2 Task N2', isDone: false},
-            {id: '3', title: 'Todolist N2 Task N3', isDone: true},
-            {id: '4', title: 'Todolist N2 Task N4', isDone: true},
-            {id: '5', title: 'Todolist N2 Task N5', isDone: true},
-            {id: '6', title: 'Todolist N2 Task N6', isDone: true},
-        ]
-    }
+    const action = addTaskAC(todolistId1, newTaskTitle)
 
-    const action = {}
+    const resultState = tasksReducer(initialState, action)
+
+    expect(resultState[todolistId1][0].title).toBe(newTaskTitle)
+    expect(resultState[todolistId1].length).toBe(3)
+})
+
+test('correct tasks status should be changed', () => {
+   const action = changeTaskStatusAC(todolistId1, '1', true)
+
+    const resultState = tasksReducer(initialState, action)
+
+    expect(resultState[todolistId1][0].isDone).toBe(true)
+})
+
+test('correct tasks title should be changed on correct title', () => {
+    const newTitle = 'Task new title (tasksReducer)'
+    const action = changeTaskTitle(todolistId1, '1', newTitle)
+
+    const resultState = tasksReducer(initialState, action)
+
+    expect(resultState[todolistId1][0].title).toBe(newTitle)
 })
--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,4 +1,5 @@
 import {TasksStateType} from "../app/App";
+import {v1} from "uuid";
 
 type RemoveTaskActionType = {
     type: 'REMOVE-TASK'
@@ -6,18 +7,70 @@ type RemoveTaskActionType = {
     taskId: string
 }
 
+type AddTaskActionType = {
+    type: 'ADD-TASK'
+    todolistId: string
+    newTaskTitle: string
+}
+
+type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS'
+    todolistId: string
+    taskId: string
+    newStatus: boolean
+}
+
+type ChangeTaskTitleActionType = {
+    type: 'CHANGE-TASK-TITLE'
+    todolistId: string
+    taskId: string
+    newTaskTitle: string
+}
+
 type ActionsType =
     | RemoveTaskActionType
+    | AddTaskActionType
+    | ChangeTaskStatusActionType
+    | ChangeTaskTitleActionType
 
 export const tasksReducer = (state: TasksStateType = {}, action: ActionsType) => {
     switch (action.type) {
         case 'REMOVE-TASK': {
             return {...state, [action.todolistId]: state[action.todolistId].filter(tsk => tsk.id !== action.taskId)}
         }
-        default: return state
+        case 'ADD-TASK': {
+            const newTask = {id: v1(), title: action.newTaskTitle, isDone: false}
+            return {...state, [action.todolistId]: [newTask, ...state[action.todolistId]]}
+        }
+        case 'CHANGE-TASK-STATUS': {
+            return {
+                ...state, [action.todolistId]: state[action.todolistId].map(tsk =>
+                    tsk.id === action.taskId ? {...tsk, isDone: action.newStatus} : tsk
+                )
+            }
+        }
+        case 'CHANGE-TASK-TITLE': {
+            return {...state, [action.todolistId]: state[action.todolistId].map(
+                    tsk => tsk.id === action.taskId ? {...tsk, title: action.newTaskTitle} : tsk
+                )}
+        }
+        default:
+            return state
     }
 }
 
-export const removeTaskAC = (todolistId: string, taskId: string) => {
+export const removeTaskAC = (todolistId: string, taskId: string): RemoveTaskActionType => {
     return {type: 'REMOVE-TASK', todolistId, taskId} as const
+}
+
+export const addTaskAC = (todolistId: string, newTaskTitle: string): AddTaskActionType => {
+    return {type: 'ADD-TASK', todolistId, newTaskTitle} as const
+}
+
+export const changeTaskStatusAC = (todolistId: string, taskId: string, newStatus: boolean): ChangeTaskStatusActionType => {
+    return {type: 'CHANGE-TASK-STATUS', todolistId, taskId, newStatus} as const
+}
+
+export const changeTaskTitle = (todolistId: string, taskId: string, newTaskTitle: string): ChangeTaskTitleActionType => {
+    return {type: 'CHANGE-TASK-TITLE', todolistId, taskId, newTaskTitle} as const
 }
